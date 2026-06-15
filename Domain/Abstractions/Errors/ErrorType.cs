b8ec25eb2a4c27fdff9f013c9f8e63b1490526b0
Muhammad Tariq <muhammadtariq72623@gmail.com>
2026-06-15 15:52:05 +0500

    set domain layer

---
+++ Domain/Abstractions/Errors/ErrorType.cs
@@ -0,0 +1,13 @@
+namespace Domain.Abstractions.Errors;
+
+public enum ErrorType
+{
+    Failure = 0,
+    Unexpected = 1,
+    Validation = 2,
+    Conflict = 3,
+    NotFound = 4,
+    Unauthorized = 5,
+    Forbidden = 6,
+    Custom = 7
+}
---
+++ Domain/Entities/Entity.cs
@@ -0,0 +1,8 @@
+﻿namespace Domain.Entities;
+
+public class Entity
+{
+    public Guid Type { get; set; }
+    public DateTime CreatedOn { get; set; } = DateTime.UtcNow;
+    public DateTime? UpdatedOn { get; set; } = null;
+}
